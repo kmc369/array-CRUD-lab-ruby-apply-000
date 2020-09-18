10c3e597e79bb545070f795376b95cf2ec56bc4c
keisha coleman <keishacoleman710@gmail.com>
2020-09-18 02:09:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,7 +1,7 @@
-
+bob = ["Bob", 46, "Juggler", "555-555-5555"]
 def movearrays(array)
 puts "name : #{array[0]}, height: #{array[1]}, last name : #{array[2]}"
 
 end
 
-movearrays()
+movearrays(bob)
